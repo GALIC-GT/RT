d6017f1c4c7ae9bb649219c5b93dc59b5f00813e
ORGP <327248022+GALIC-GT@users.noreply.github.com>
2026-09-15 22:11:30 -0600

    Se añadio la funcionalidad de girar la moneda y tambien de mostrar el grafico

--- Root/app.py
+++ Root/app.py
@@ -4,29 +4,42 @@ import time
 
 st.header('Lanzar una moneda')
 
-chart = st.line_chart([0.5])
+chart = st.empty()
+
 
 def toss_coin(n):
-
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
     mean = None
     outcome_no = 0
     outcome_1_count = 0
+    means = []
 
     for r in trial_outcomes:
-        outcome_no +=1
+        outcome_no += 1
+
         if r == 1:
             outcome_1_count += 1
+
         mean = outcome_1_count / outcome_no
-        chart.add_rows([mean])
+        means.append(mean)
+
+        chart.line_chart(means)
+
         time.sleep(0.05)
 
     return mean
 
+
 number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+
 start_button = st.button('Ejecutar')
 
 if start_button:
-    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    st.write(
+        f'Experimento con {number_of_trials} intentos en curso.'
+    )
+
     mean = toss_coin(number_of_trials)
+
+    st.write(f'Proporción final de caras: {mean}')
