a2d903c8a9d34d155cd625d38153f4a5c133fffc
ORGP <327248022+GALIC-GT@users.noreply.github.com>
2026-09-15 22:19:08 -0600

    correcion de errores

--- Root/app.py
+++ Root/app.py
@@ -1,17 +1,22 @@
 import scipy.stats
 import streamlit as st
 import time
+import pandas as pd
 
-# estas son variables de estado que se conservan cuando Streamlin vuelve a ejecutar este script
-if 'experiment_no' not in st.session_state:
-    st.session_state['experiment_no'] = 0
-
-if 'df_experiment_results' not in st.session_state:
-    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iteraciones', 'media'])
 
 st.header('Lanzar una moneda')
 
 chart = st.empty()
+
+
+# Inicializar variables de session_state
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(
+        columns=['no', 'iteraciones', 'media']
+    )
 
 
 def toss_coin(n):
@@ -38,22 +43,46 @@ def toss_coin(n):
     return mean
 
 
-number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+number_of_trials = st.slider(
+    '¿Número de intentos?',
+    1,
+    1000,
+    10
+)
 
 start_button = st.button('Ejecutar')
 
+
 if start_button:
-   st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    st.write(
+        f'Experimento con {number_of_trials} intentos en curso.'
+    )
+
     st.session_state['experiment_no'] += 1
+
     mean = toss_coin(number_of_trials)
-    st.session_state['df_experiment_results'] = pd.concat([
-        st.session_state['df_experiment_results'],
-        pd.DataFrame(data=[[st.session_state['experiment_no'],
-                            number_of_trials,
-                            mean]],
-                     columns=['no', 'iteraciones', 'media'])
+
+    st.session_state['df_experiment_results'] = pd.concat(
+        [
+            st.session_state['df_experiment_results'],
+            pd.DataFrame(
+                data=[
+                    [
+                        st.session_state['experiment_no'],
+                        number_of_trials,
+                        mean
+                    ]
+                ],
+                columns=['no', 'iteraciones', 'media']
+            )
         ],
-        axis=0)
-    st.session_state['df_experiment_results'] = st.session_state['df_experiment_results'].reset_index(drop=True)
+        axis=0
+    )
+
+    st.session_state['df_experiment_results'] = (
+        st.session_state['df_experiment_results']
+        .reset_index(drop=True)
+    )
+
 
 st.write(st.session_state['df_experiment_results'])
